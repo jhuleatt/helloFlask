2a77ed90cff015049e6ac0b368c72ba1106a0f06
Jeffrey Huleatt <jhuleatt@scarletmail.rutgers.edu>
2015-01-01 17:42:20 -0800

    better idea for turning light on and off

--- hello-template.py
+++ hello-template.py
@@ -5,18 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    try:
-        if lightOn == True:
-            lightOn = False
-        else:
-            lightOn = True
-    except UnboundLocalError:
-        lightOn = True
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(7, GPIO.OUT)
-    GPIO.output(7,lightOn)
-
     now = datetime.datetime.now()
     timeString = now.strftime("%Y-%m-%d %H:%M")
     templateData = {
@@ -24,6 +12,22 @@ def hello():
       'time': timeString
       }
     return render_template('main.html', **templateData)
+
+@app.route("/on")
+def turnOn():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.output(7,True)
+
+    return "Light is on"
+
+@app.route("/off")
+def turnOff():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.output(7,False)
+
+    return "Light is off"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
